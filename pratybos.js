8d1205fa0ec6edf132c3898a96cae527dea2941b
kestulka <kgirdiunas0@gmail.com>
2023-11-09 00:40:42 +0200

    meh

---
+++ pratybos.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Pratybos (1dalis 70skaidre)
+
+// 1.
+let miau = "katinas";
+// 1. a.
+miau = 13;
+// 1. b.
+console.log(miau);
+
+// 1.
+let didinam = 3;
+console.log(didinam);
+console.log(didinam++);
+console.log(didinam++);
+console.log(didinam++);
+console.log(didinam);
+
+// 2.
+let mazinam = 8;
+console.log(mazinam--);
+console.log(mazinam--);
+console.log(mazinam--);
+console.log(mazinam);
+
+// 3.
+let keiciam = "23";
+
+keiciam = +"23";
+console.log(keiciam);
+
+keiciam = Number("23");
+console.log(keiciam);
+
+keiciam = "23" * 1;
+console.log(keiciam);
+
+// 1.
+let pirmasSkaicius = 3;
+// 1. a.
+let antrasSkaicius = pirmasSkaicius + 5;
+console.log(antrasSkaicius);
